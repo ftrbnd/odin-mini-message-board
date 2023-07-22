1e5e58b9f09c46f2338cf16161da46e7edc38045
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-07-21 21:38:25 -0700

    Add new message form and redirects

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,30 @@ const express = require('express');
 const router = express.Router();
 
 const messages = [
-   {
-     text: "Hi there!",
-     user: "Amando",
-     added: new Date()
-   },
-   {
-     text: "Hello World!",
-     user: "Charles",
-     added: new Date()
-   }
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date()
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date()
+  }
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: "Mini Message Board", messages: messages });
+router.get('/', function (_req, res, _next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.post('/new', function (req, res, _next) {
+  const author = req.body.author;
+  const message = req.body.message;
+
+  messages.push({ text: message, user: author, added: new Date() });
+
+  res.redirect('/');
 });
 
 module.exports = router;
